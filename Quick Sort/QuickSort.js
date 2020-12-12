04be62501624deb1c78970ef825df6ceb23498f2
Jonathan Martinez <68358371+jmartin-code@users.noreply.github.com>
2020-12-11 22:32:08 -0500

    quicksort with pivot index

--- Quick Sort/QuickSort.js
+++ Quick Sort/QuickSort.js
@@ -1,9 +1,40 @@
 //Quick Sort
-// Works by selecting one element called the pivot and finding the index where the pivot
+// Works by selecting a pivot element and finding the index where the pivot
 // should end up in the sorted Array. Elements lower that the pivot are placed in the screen Left
 // elements higher than the pivot are placed in the right. the pivot is positioned/sorted and the 
 //process is repeated. 
 
-console.log("hola");
-console.log("dimelo");
+const quickSort = (arr, start=0, end=arr.length+1)=>{
+    const swap = (array, i, j) => {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
+    let pivot = arr[start];
+    let swapId = start;
 
+    for(let i=start + 1; i<arr.length; i++){
+        if(pivot > arr[i]){
+            swapId++;
+            swap(arr, swapId, i);
+        }
+    }
+    swap(arr, start, swapId);
+    return swapId;
+}
+
+function pivotQuickSort(arr, left = 0, right = arr.length-1){
+    //stop
+    if(left < right){
+        let pivotId = quickSort(arr, left, right);
+        //left side
+        pivotQuickSort(arr, left, pivotId -1);
+    
+        //right side
+        pivotQuickSort(arr, pivotId +1, right);
+    }
+    console.log (arr);
+}
+
+pivotQuickSort([4,8,2,1,5,7,6,3]);
