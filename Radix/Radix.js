8fdeca98ffbd3059413dbba11665c7eb30e247a7
Jonathan Martinez <68358371+jmartin-code@users.noreply.github.com>
2020-12-13 10:44:54 -0500

    Radix

--- Radix/Radix.js
+++ Radix/Radix.js
@@ -3,9 +3,24 @@
 //this sort doesnt compare the numbers.
 //radix sort is fast.
 
-
+//method to get digits from whole number
 const getDigit = (num, place) =>{
     console.log( Math.floor(Math.abs(num) / Math.pow(10, place)) % 10);
+}
+
+// count the number of digits in a number
+const digitCount = (num) => {
+    if(num === 0) return 1;
+    return Math.floor(Math.log10 (Math.abs(num))) +1;
+}
+
+//
+const mostDigits = (nums) => {
+    let maxDigits = 0;
+    for (let i = 0; i< nums.length; i++){
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits
 }
 
 getDigit(12345, 5)
