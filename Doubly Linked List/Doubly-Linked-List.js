549ade1492be38838aae596d1ad064a06ce036df
Jonathan Martinez <68358371+jmartin-code@users.noreply.github.com>
2021-02-15 00:40:43 -0500

    working on the doubly linked list

--- Doubly Linked List/Doubly-Linked-List.js
+++ Doubly Linked List/Doubly-Linked-List.js
@@ -1,3 +1,23 @@
+//Definition:
 //Doubly linked list has two pointer instead of one, as in singly line list.
 //one pointer points to the next item, the other points to the previous item.
+//Very similar to singly linked list.
 //However, doubly linked list takes more merory
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+//add node to the end of the list
